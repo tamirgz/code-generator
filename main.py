9f632f4626d40bc3cd4e51f3f2ee2b77c37f07d7
Tamir Gazneli <tamirgz@gmail.com>
2025-03-01 19:47:19 +0200

    Adding code-generator code to local_code_generator branch that enables execution locally in python

---
+++ main.py
@@ -0,0 +1,10 @@
+from CodeGeneration import CodeGenerationWorkflow
+from typing import Iterator
+from agno.agent import RunResponse
+from agno.utils.pprint import pprint_run_response
+
+# Run workflow
+workflow: Iterator[RunResponse] = CodeGenerationWorkflow().run_workflow(user_input="Generate python code create agents using agno python package that builds a workflow to generate news reports based on articles from a given URL")
+
+# Print the report
+pprint_run_response(workflow, markdown=True, show_time=True)
